5763d21897e2f8d784179cb82d377d7bbf8fe0e5
AidanJohnston <aidanjhnston@gmail.com>
2021-12-05 20:05:38 -0500

    new database strucutre

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -1,7 +1,13 @@
-CREATE TABLE users {
+CREATE TABLE users (
     uid int NOT NULL AUTO_INCREMENT,
     user_name varchar(255) NOT NULL,
     email varchar(255),
     password varchar(255) NOT NULL,
     PRIMARY KEY (uid)
-}
+)
+
+CREATE TABLE sessions (
+    uid varchar(23) NOT NULL,
+    code varchar(6) NOT NULL,
+    PRIMARY KEY(uid)
+)
