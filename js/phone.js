2c50bdec5d3e2676ef99c6a2b2eefaf0fe2e7714
AidanJohnston <aidanjhnston@gmail.com>
2021-12-05 23:38:07 -0500

    display code

--- js/phone.js
+++ js/phone.js
@@ -1,18 +1,17 @@
 import QrScanner from "./qr-scanner.min.js";
 QrScanner.WORKER_PATH = "../js/qr-scanner-worker.min.js";
+var videoElem = document.getElementById('qrscannervideo')
+const qrScanner = new QrScanner(videoElem, result => handleCode(result));
 
 window.onload = function() {
-    var videoElem = document.getElementById('qrscannervideo')
-    const qrScanner = new QrScanner(videoElem, result => handleCode(result));
-
     qrScanner.start();
 }
 
 function handleCode(result) {
-    
+    qrScanner.stop();
 	var xmlhttp = new XMLHttpRequest();
     xmlhttp.open("POST", "../php/phone.php", true);
-
+    xmlhttp.setRequestHeader("content-type","application/x-www-form-urlencoded");
 	xmlhttp.onreadystatechange = function () {
 		//If Request successful
 		if (this.readyState == 4 && this.status == 200) {
@@ -21,9 +20,14 @@ function handleCode(result) {
             var response = this.responseText;
 
             if (response) {
-                console.log(this.response)
+                var code = document.getElementById('code');
+                code.innerHTML = "<p>" + response + "</p>";
+                
             }
 		}
+        else {
+            qrScanner.start();
+        }
 	};
 	xmlhttp.send('qr='+result);
 }
--- php/phone.php
+++ php/phone.php
@@ -4,9 +4,11 @@ include("connect.php");
 
 $conn = connect();
 
-//$sql = 'SELECT code FROM sessions WHERE uid = \'$_POST["qr"]\'';
+$qr = mysqli_real_escape_string($conn, $_POST["qr"]);
 
-//$result = mysqli_query($conn,$sql);
+$sql = "SELECT code FROM sessions WHERE uid = '$qr'";
 
-echo $_POST;
+$result = mysqli_query($conn,$sql);
+
+echo mysqli_fetch_all($result)[0][0];
 ?>
