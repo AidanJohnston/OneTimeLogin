6df7cf0503cd3ef944b53915ebb606d77404f3db
AidanJohnston <aidanjhnston@gmail.com>
2021-12-05 20:05:16 -0500

    generate qr code

---
+++ php/GenerateQR.php
@@ -0,0 +1,25 @@
+<?php
+include ("qr/qrlib.php");
+include("connect.php");
+
+// generate unique id and random key
+$id = uniqid("", true);
+$key = random_int(0, 999999);
+$key = str_pad($key, 6, 0, STR_PAD_LEFT);
+
+$conn = connect();
+
+$sql = "INSERT INTO sessions
+        VALUES ('$id', '$key')";
+
+$result = mysqli_query($conn,$sql);
+
+ob_start();
+QRcode::png($id, false, 'L', 10, 2); 
+$png = base64_encode( ob_get_contents() );
+ob_end_clean();
+
+echo $png;
+
+$conn->close();
+?>
--- php/login.php
+++ php/login.php
@@ -1,7 +1,23 @@
 <?php
 
-    
 
+$conn = connect();
 
+//$sql = "SELECT pwd FROM users WHERE user_name = {$_POST['username']}";
+
+//$result = mysqli_query($conn,$sql);
+
+//echo json_encode(mysqli_fetch_all($result));
+echo "test";
+foreach ($_GET as $value)
+    echo $value;
+echo "test";
+foreach ($_POST as $value)
+    echo $value;
+echo "test";
+foreach ($_REQUEST as $value)
+    echo $value;
+
+$conn->close();
 
 ?>
